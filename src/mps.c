3b05230c40b47add07226aa5b7ddf00e6f7ed0d3
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2022-06-15 13:15:50 +0300

    Draw two moving squares

--- src/mps.c
+++ src/mps.c
@@ -6,28 +6,13 @@
 
 #include <SDL2/SDL.h>
 
+#include <rectangle.h>
+
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-const float FPS = 30.0;
-
-int draw_vertical_line(
-	Canvas_t *canvas, const Point2D_t *start, const Color_t *color)
-{
-	Point2D_t point = *start;
-
-	for (int i = 0; i < 100; i++) {
-		if (canvas_draw_point2d(canvas, &point, color) != 0) {
-			canvas_delete(canvas);
-			return -1;
-		}
-
-		point.y++;
-	}
-
-	return 0;
-}
+const float FPS = 60.0;
 
 int main(void)
 {
@@ -35,10 +20,12 @@ int main(void)
 	SDL_Event e;
 
 	double speed = 10;
-	Point2D_t point = {20, 100};
 	Color_t black = {0x00, 0x00, 0x00, 0xFF};
-	//Color_t white = {0xFF, 0xFF, 0xFF, 0xFF};
+	Color_t white = {0xFF, 0xFF, 0xFF, 0xFF};
 	Color_t red = {0xFF, 0x00, 0x00, 0xFF};
+
+	Rectangle_t rect_1 = {{20, 20}, 100, 100};
+	Rectangle_t rect_2 = {{50, 50}, 100, 100};
 
 	Canvas_t *canvas = canvas_create(SCREEN_WIDTH, SCREEN_HEIGHT);
 	if (canvas == NULL)
@@ -55,8 +42,11 @@ int main(void)
 			return -1;
 		}
 
-		draw_vertical_line(canvas, &point, &red);
-		point.x = (unsigned)(point.x + speed) % SCREEN_WIDTH;
+		canvas_draw_rectangle(canvas, &rect_2, &white);
+		canvas_draw_rectangle(canvas, &rect_1, &red);
+
+		rect_1.start.x = (int)(rect_1.start.x + speed) % SCREEN_WIDTH;
+		rect_2.start.x = (int)(rect_2.start.x + 2*speed) % SCREEN_WIDTH;
 
 		canvas_update(canvas);
 		SDL_Delay(1000 / FPS);
