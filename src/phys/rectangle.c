51c961dd0de80cc0c47e22cd994b84e174028ab2
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2022-07-03 00:55:41 +0300

    PhysRectangle: use vector math instead of direct access to coordinates

--- src/phys/rectangle.c
+++ src/phys/rectangle.c
@@ -2,6 +2,7 @@
 
 void phys_rectangle_move(PhysRectangle_t *rect, double dt)
 {
-	rect->start.x += rect->velocity.x * dt;
-	rect->start.y += rect->velocity.y * dt;
+	Vector2D_t dx = {0};
+	vector2d_mul_float(&dx, &rect->velocity, dt);
+	vector2d_sum(&rect->start, &rect->start, &dx);
 }
