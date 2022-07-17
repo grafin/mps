3703228776cb7265b65af72f451718dfc8ee0004
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2022-07-17 20:16:51 +0300

    Universe: refactor to conform with checkpatch

--- include/phys/universe.h
+++ include/phys/universe.h
@@ -1,13 +1,23 @@
 #ifndef UNIVERSE_H_
 #define UNIVERSE_H_
 
-typedef struct Universe {
+/**
+ * Strcut that describes physical universe of the simulation.
+ */
+struct Universe {
 	double left_boundary, right_boundry, top_boundary, bottom_boundary;
-} Universe_t;
+};
 
-extern Universe_t universe;
+/**
+ * Universe for the simulation.
+ */
+extern struct Universe universe;
 
-void universe_init(double left_boundary, double right_boundry,
-		   double top_boundary, double bottom_boundary);
+/**
+ * Initialize physical universe for the simulation.
+ */
+void
+universe_init(double left_boundary, double right_boundry,
+	      double top_boundary, double bottom_boundary);
 
 #endif /* UNIVERSE_H_ */
--- src/phys/universe.c
+++ src/phys/universe.c
@@ -1,9 +1,10 @@
 #include <phys/universe.h>
 
-Universe_t universe;
+struct Universe universe;
 
-void universe_init(double left_boundary, double right_boundry,
-		  double top_boundary, double bottom_boundary)
+void
+universe_init(double left_boundary, double right_boundry,
+	      double top_boundary, double bottom_boundary)
 {
 	universe.left_boundary = left_boundary;
 	universe.right_boundry = right_boundry;
